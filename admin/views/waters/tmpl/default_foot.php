2621309f7ee3f574100a7f6dd3a3b9b3bde803f7
Shibin Roman <rsa_m@mail.ru>
2012-10-28 23:28:32 +0400

    Добавление информации об авторе и добровольных пожертвованиях

--- admin/views/waters/tmpl/default_foot.php
+++ admin/views/waters/tmpl/default_foot.php
@@ -3,5 +3,49 @@
 defined('_JEXEC') or die('Restricted Access');
 ?>
 <tr>
-	<td></td>
+	<td>
+
+	<table width='100%'>
+		<tr>
+			<td width='50%' class='adminform' valign='top'>
+
+			<div class='clr'></div>
+			</td>
+			<td valign='top' width='50%' style='padding: 0px 0px 0px 0px'>
+			<table class='adminlist'>
+				<tr>
+					<td colspan='2'>
+					<p><?php echo JText::_('Компонент УК и ТСЖ для CMS Joomla 2.5')?></p>
+					</td>
+				</tr>
+				<tr>
+					<td width='40%'><?php echo JText::_('Version'); ?></td>
+					<td width='60%'><?php echo '0.1.0'; ?></td>
+				</tr>
+				<tr>
+					<td><?php echo JText::_('Copyright'); ?></td>
+					<td><a href='https://github.com/rsam/com_tsj' target='_blank'>&copy;
+					2012 Shibin Roman </a></td>
+				</tr>
+				<tr>
+					<td><?php echo JText::_('License'); ?></td>
+					<td><a href='http://www.gnu.org/licenses/gpl-3.0.html'
+						target='_blank'>GNU/GPL v3</a></td>
+				</tr>
+				<tr>
+					<td><?php echo JText::_('Donate'); ?></td>
+					<td>Для поддержки развития компонента Вы можете
+					   сделать добровольное пожертвование денежных средств.<br>
+					   <a href='http://www.webmoney.ru/'
+						target='_blank'>WMZ:Z252490757879</a><br>
+						<a href='http://www.webmoney.ru/'
+                  target='_blank'>WMR:R159357132082</a><br>
+                  Спасибо за поддержку.</td>
+				</tr>
+			</table>
+			</td>
+		</tr>
+	</table>
+
+	</td>
 </tr>
