ed24505c8ca87eb025575c5f39cea431e3bd62a9
rsa <rsa_m@mail.ru>
2015-11-07 12:04:06 +0300

    Fix bug with Regex Add counter inport

--- admin/models/rules/name.php
+++ admin/models/rules/name.php
@@ -17,5 +17,16 @@ class JFormRuleName extends JFormRule
 	 * @var		string
 	 * @since	2.5
 	 */
-	protected $regex = '^[A-zÐ-Ñ0-9_^\,./-\s]+$';
+	//protected $regex = '^[A-zÐ-Ñ0-9_^\,./-\s]+$';
+
+public function test(SimpleXMLElement $element, $value, $group = null, JRegistry $input = null, JForm $form = null)
+    {
+        if(!preg_match('/[A-z0-9À-ÿ_^\,\.\-]+/i', $value)) {
+            $element->addAttribute('message', 'Field may only contain A-z À-ÿ 0-9 _^,.-');
+            return false;
+        }/*elseif(!$somethingelse) {
+            $element->addAttribute('message', 'Something else is wrong');
+            return false;
+        }*/ 
+    }
 }
